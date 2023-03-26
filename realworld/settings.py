a487924c4a17196b5ff7e1e16e071147c4d4d91e
Hyemin Lee <hmin10580@gmail.com>
2023-03-26 21:07:07 +0900

    Temp DB settings

--- realworld/settings.py
+++ realworld/settings.py
@@ -82,13 +82,9 @@ WSGI_APPLICATION = "realworld.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
-    },
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
 
 # Password validation
